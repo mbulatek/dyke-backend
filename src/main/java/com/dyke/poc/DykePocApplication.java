4256cf6d55f521e7912a01c150a8bde60e2b592f
skrzychuz@wp.pl <skrzychuz@wp.pl>
2018-02-03 19:03:54 +0100

    Initial commit

---
+++ src/main/java/com/dyke/poc/DykePocApplication.java
@@ -0,0 +1,14 @@
+package com.dyke.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DykePocApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DykePocApplication.class, args);
+  }
+}
+
+
