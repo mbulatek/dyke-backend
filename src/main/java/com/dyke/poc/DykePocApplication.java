c4590f6f1c97df04980abdddf3441fd41fd76f9e
Marek Bulatek <mbulatek@gmail.com>
2018-06-22 22:13:36 +0200

    maly test

--- src/main/java/com/dyke/poc/DykePocApplication.java
+++ src/main/java/com/dyke/poc/DykePocApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DykePocApplication {
 
   public static void main(String[] args) {
+    // maly test
     SpringApplication.run(DykePocApplication.class, args);
   }
 }
